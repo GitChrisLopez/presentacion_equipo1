18f7a839ccb6a9ff4196249eb1a4700a2ab85264
Chrizz <sabine.fitch252379@potros.itson.edu.mx>
2025-04-01 23:20:54 -0700

    Se agrego el DTO necesario para el Inicio Sesion de reclutadores

--- src/presentacion_equipo1/RevisionCV/IniciarSesionRH.java
+++ src/presentacion_equipo1/RevisionCV/IniciarSesionRH.java
@@ -29,8 +29,8 @@ public class IniciarSesionRH extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabelFotoRH = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        TextFieldUsuarioRecursosHumanos = new javax.swing.JTextField();
-        jPasswordRecursosHumanos = new javax.swing.JPasswordField();
+        txtUser = new javax.swing.JTextField();
+        txtContra = new javax.swing.JPasswordField();
         jButtonIniciarSesionRH = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -44,9 +44,9 @@ public class IniciarSesionRH extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 18)); // NOI18N
         jLabel1.setText("¡Bienvenido Reclutador!");
 
-        TextFieldUsuarioRecursosHumanos.addActionListener(new java.awt.event.ActionListener() {
+        txtUser.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TextFieldUsuarioRecursosHumanosActionPerformed(evt);
+                txtUserActionPerformed(evt);
             }
         });
 
@@ -82,13 +82,13 @@ public class IniciarSesionRH extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createSequentialGroup()
                                     .addComponent(jLabel3)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(jPasswordRecursosHumanos))
+                                    .addComponent(txtContra))
                                 .addGroup(jPanel1Layout.createSequentialGroup()
                                     .addComponent(jLabel2)
                                     .addGap(31, 31, 31)
-                                    .addComponent(TextFieldUsuarioRecursosHumanos, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGap(9, 9, 9))))
-                .addContainerGap(62, Short.MAX_VALUE))
+                .addContainerGap(68, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jButtonIniciarSesionRH)
@@ -103,12 +103,12 @@ public class IniciarSesionRH extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(24, 24, 24)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(TextFieldUsuarioRecursosHumanos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel3)
-                    .addComponent(jPasswordRecursosHumanos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtContra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
                 .addComponent(jButtonIniciarSesionRH, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(41, 41, 41))
@@ -118,9 +118,7 @@ public class IniciarSesionRH extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 6, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -130,9 +128,9 @@ public class IniciarSesionRH extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void TextFieldUsuarioRecursosHumanosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextFieldUsuarioRecursosHumanosActionPerformed
+    private void txtUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUserActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_TextFieldUsuarioRecursosHumanosActionPerformed
+    }//GEN-LAST:event_txtUserActionPerformed
 
     private void jButtonIniciarSesionRHActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonIniciarSesionRHActionPerformed
         // TODO add your handling code here:
@@ -177,13 +175,13 @@ public class IniciarSesionRH extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField TextFieldUsuarioRecursosHumanos;
     private javax.swing.JButton jButtonIniciarSesionRH;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabelFotoRH;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordRecursosHumanos;
+    private javax.swing.JPasswordField txtContra;
+    private javax.swing.JTextField txtUser;
     // End of variables declaration//GEN-END:variables
 }
