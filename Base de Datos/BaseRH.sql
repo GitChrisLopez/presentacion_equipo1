ccb465cc8cf293d785a356b6df0ae725bc85f93d
Adrián Castán <151794030+adcastan@users.noreply.github.com>
2025-05-12 16:26:22 -0700

    Añadida base de datos
    Ahi esta el documento para que le muevan, ahi ponen los Stored Procedures y todo

---
+++ Base de Datos/BaseRH.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE apprh;
+USE apprh;
+
+CREATE TABLE reclutadores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellidoPaterno VARCHAR(50),
+    apellidoMaterno VARCHAR(50),
+    puesto VARCHAR(50),
+    usuario VARCHAR(50) UNIQUE,
+    contrasena VARCHAR(100),
+    estado BOOLEAN
+);
+
+CREATE TABLE candidatos(
+	id int auto_increment primary key,
+    nombre VARCHAR(100),
+    apellidoPaterno VARCHAR(50),
+    apellidoMaterno VARCHAR(50),
+    puesto VARCHAR(50),
+    estado boolean,
+	rutapdf text
+);
+
+
